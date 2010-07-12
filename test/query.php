ba4697df5c6fb41c946e71eae0ccf2e6446fc4e0
nichtich <jakob@nichtich.de>
2010-07-12 23:45:01 +0200

    added testing framework based on SimpleTest

---
+++ test/query.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once(__DIR__.'/test.php'); // defines TestBase
+
+require_once(__DIR__.'/../library/Wigit/Query.php');
+
+class QueryTest extends TestBase {
+
+    function testCreate() {
+        $this->assertTrue(true);
+    }
+}
+
+?>
